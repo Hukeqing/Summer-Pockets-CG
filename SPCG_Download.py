995338f1e38d043ba6af0a8ca4364dcc23f4d4e8
HuKeqing <keqing.hu@icloud.com>
2019-07-15 19:58:17 +0800

    Update

--- SPCG_Download.py
+++ SPCG_Download.py
@@ -401,15 +401,26 @@ http://a4.qpic.cn/psb?/V12gDDWR0Yw3qy/Nmp0CHYL2D4Vi*3hYI9aVloD*f28JIVJAnDbgk1qGj
 
 source = source.split('\n')
 mode = 0
+sleeptime = 5
+print('******************************************************************')
+print('Create by Mauve')
+print('我的GitHub：https://github.com/Hukeqing')
+print('我的bilibili：https://space.bilibili.com/175413312')
+print()
+print('所有的CG仅用于交流，学习和分享，')
+print('严禁未经官方授权的任何商业用途，')
+print('若因此引起的版权纠纷，本人不对所造成的后果负责。')
+print('******************************************************************')
+print()
 try:
     mode = int(input('请输入需要的下载方式\n1、全部下载\n2、按页下载\n3、区间下载\n请输入编号并按下回车：'))
 except ValueError:
     print('错误!不是数字，程序终止')
-    time.sleep(10)
+    time.sleep(sleeptime)
     exit(0)
 if mode < 1 or mode > 3:
     print('错误，超出范围，程序终止')
-    time.sleep(10)
+    time.sleep(sleeptime)
     exit(0)
 savepath = input('请输入文件保存的地址，按下回车确定(留空则保存到当前目录下)：')
 if savepath == "":
@@ -417,7 +428,7 @@ if savepath == "":
 
 if not os.path.isdir(savepath):
     print('这不是一个目录，程序终止！')
-    time.sleep(10)
+    time.sleep(sleeptime)
     exit(0)
 
 
@@ -441,12 +452,12 @@ elif mode == 2:
     try:
         pages = int(input('请输入要下载的页码（仅数字，一个整数，每一页默认下载40个图片）\n例如第一页下载 1 - ' + str(photoperpage) + ' 的图片'))
     except ValueError:
-        print('错误!不是数字，程序终止')
-        time.sleep(10)
+        print('错误！不是数字，程序终止')
+        time.sleep(sleeptime)
         exit(0)
     if pages < 0 or pages > maxpage:
-        print('错误，超出范围，程序终止')
-        time.sleep(10)
+        print('错误！超出范围，程序终止')
+        time.sleep(sleeptime)
         exit(0)
     first = pages * photoperpage + 1
     end = first + photoperpage - 1
@@ -454,24 +465,26 @@ else:
     try:
         first = int(input('请输入要下载的起始编号（1-' + str(maxphoto) + '）：'))
     except ValueError:
-        print('错误!不是数字，程序终止')
-        time.sleep(10)
+        print('错误！不是数字，程序终止')
+        time.sleep(sleeptime)
         exit(0)
     if first < 0 or first > maxphoto:
-        print('错误，超出范围，程序终止')
-        time.sleep(10)
+        print('错误！超出范围，程序终止')
+        time.sleep(sleeptime)
         exit(0)
     try:
         end = int(input('请输入要下载的结束编号（' + str(first) + '-' + str(maxphoto) + '）：'))
     except ValueError:
-        print('错误!不是数字，程序终止')
-        time.sleep(10)
+        print('错误！不是数字，程序终止')
+        time.sleep(sleeptime)
         exit(0)
     if end < first or end > maxphoto:
-        print('错误，超出范围，程序终止')
-        time.sleep(10)
+        print('错误！超出范围，程序终止')
+        time.sleep(sleeptime)
         exit(0)
 
 while first <= end:
     download(first)
     first += 1
+print('下载完成！')
+time.sleep(sleeptime)
