6772070840ee35335cb7540b9c77efe8f360ae0f
HuKeqing <keqing.hu@icloud.com>
2019-07-18 00:51:52 +0800

    Update

--- makehtml.py
+++ makehtml.py
@@ -7,7 +7,7 @@ htmlfile = open('CGpage/' + str(curid) + '.html', 'w')
 htmlfile.write('<!DOCTYPE html>\n<html>\n\n<head>\n\t<meta charset="utf-8">\n\t<title>page ' + str(curid) + '</title>\n\t<link rel="stylesheet" type="text/css" media="screen" href="main.css">\n</head>\n\n<body>\n\t<div>\n\t\t<iframe frameborder="no" id="bgm" class="bgm"\n\t\t\tsrc="https://music.163.com/outchain/player?type=2&id=' + str(bgmlist[curid - 1]) +'&auto=0&height=66">\n\t\t</iframe>\n\t</div>\n\t<div>\n')
 for line in sourcefile:
     maincount += 1
-    htmlfile.write('\t\t<div class="img">\n\t\t\t<div class="tag">' + str(maincount) + '</div><a href="' + line + '" target="_blank"><img class="photo" src="' + line + '"> </a> </div>\n')
+    htmlfile.write('\t\t<div class="img">\n\t\t\t<div class="tag">' + str(maincount) + '</div><a href="' + line + '" target="_blank"><img class="photo" src="' + line + '"> </a>\n\t\t</div>\n')
     count += 1
     if count == 40:
         htmlfile.write('\t</div>\n</body>\n\n</html>')
